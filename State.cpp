b4cf5cd5515877e49b99e641e8f95074c1081da1
jkdarklord <juan-ca-pq@hotmail.com>
2015-09-21 16:51:45 -0600

    Clase State separada de Processor.cpp

---
+++ State.cpp
@@ -0,0 +1,24 @@
+#ifndef STATE_CPP
+#define STATE_CPP
+
+#include <stdlib.h>
+// Clase encargada de llevar los registros y el estado del pipeline del procesador 
+class State{
+    public:
+        // Registros
+        int * registers;
+        int pc;
+        int rl;
+        // Estados de pipeline
+        // Constructor
+        State(){
+                registers = (int *) calloc (32*sizeof(int),0);
+                pc=0;
+                rl=0;
+        }
+        // Destructor
+        ~State(){
+                 free(registers);
+        }
+};
+#endif
