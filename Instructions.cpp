b9f5383bd0dd48304498888a89a2752ca0beca44
jkdarklord <juan-ca-pq@hotmail.com>
2015-09-21 16:03:40 -0600

    Create Instructions.cpp

---
+++ Instructions.cpp
@@ -0,0 +1,19 @@
+#ifndef INSTRUCTIONS_CPP
+#define INSTRUCTIONS_CPP
+// Clase encargada de las instrucciones MIPS
+
+#include "Processor.cpp"
+
+class Instructions{
+    private:
+        //
+    public:
+        // Constructor
+        Instructions(){
+            
+        }
+        // Destructor
+        ~Instructions(){
+        }
+};
+#endif
