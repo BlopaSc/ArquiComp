d1c2bfce6160bcf5b49fc096c907b0c6b99563f6
jkdarklord <juan-ca-pq@hotmail.com>
2015-09-21 16:11:07 -0600

    Update Processor.cpp

--- Processor.cpp
+++ Processor.cpp
@@ -1,32 +1,41 @@
-#ifndef PROCESSOR.CPP
+#ifndef PROCESSOR_CPP
 #define PROCESSOR_CPP
+
+#include <stdlib.h>
 // Clase encargada de llevar los registros y el estado del pipeline del procesador 
 class State{
     public:
+        friend class Processor;
         // Registros
+        int * registers;
         // Estados de pipeline
         // Constructor
         State(){
+                registers = (int *) calloc (32*sizeof(int),0);
         }
         // Destructor
         ~State(){
+                 free(registers);
         }
 };
 
 // Clase que emula un procesador
 class Processor{
     private:
-        State* state;
         long long cycles;
         unsigned char flags;
     public:
+           friend class State;
+           State* state;
         // Constructor
         Processor(){
             cycles=0;
             flags=0x0;
+            state = new State();
         }
         // Destructor
         ~Processor(){
+            delete state;
         }
         // Retorna si el procesador se encuentra en un estado de fin
         inline unsigned char getFin(){return flags&0x1;}
@@ -34,9 +43,8 @@ class Processor{
         inline long long getCycle(){return cycles;}
         // Retorna el estado actual del procesador para almacenarlo
         inline State* getState(){return state;}
-        // Es la señal que ocasiona la ejecucion de un nuevo ciclo de reloj
+        // Es la seÃ±al que ocasiona la ejecucion de un nuevo ciclo de reloj
         void execute(){
         }
-        
 };
 #endif
