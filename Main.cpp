6665a63680f13a47f1ea22ca3e73d0c8a1dac957
Juan Carlos Porras <juan-ca-pq@hotmail.com>
2015-09-24 16:34:14 -0600

    Agregada la lectura de archivos y envio de instrucciones a memoria

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <fstream>
 #include "ThreadQueue.cpp"
 #include "Processor.cpp"
 #include "Bus.cpp"
@@ -9,11 +10,14 @@
 #define NUM_PROCS 2
 #define QUANTUM 16
 
+using namespace std;
 pthread_barrier_t  synchroBarrier;
 Memory *mainMemory;
 Bus *instrBus, *dataBus;
 ThreadQueue *threadManager;
 pthread_t *thread;
+int instructionsProcessed;
+
 
 void *threadProcessor(void *paramPtr){
       Processor* proc = new Processor(instrBus,dataBus);
@@ -45,12 +49,45 @@ void run(){
     printf("Emulacion finalizada\n");
 }
 
+void loadFile(char * filename){
+     printf("Opening file: %s \n",filename);
+     ifstream infile(filename);
+     int codigo,p1,p2,p3;
+     while (infile >> codigo >> p1 >> p2 >> p3)
+     {
+      printf("Read: %d %d %d %d \n",codigo, p1, p2, p3);
+      mainMemory->ramInstructions[instructionsProcessed]=codigo;
+      instructionsProcessed++;
+      mainMemory->ramInstructions[instructionsProcessed]=p1;
+      instructionsProcessed++;
+      mainMemory->ramInstructions[instructionsProcessed]=p2;
+      instructionsProcessed++;
+      mainMemory->ramInstructions[instructionsProcessed]=p3;
+      instructionsProcessed++;
+     }    
+}
+
+void displayMemory(){
+     printf("Despliegue de memoria: \n");
+     for(int i=0;i<instructionsProcessed;i++){
+         printf("%d ",mainMemory->ramInstructions[i]);            
+     }
+     printf("\n");
+}
+
+
 int main(){
     mainMemory = new Memory();
     instrBus = new Bus(mainMemory->ramInstructions);
     dataBus = new Bus(mainMemory->ramData);
     threadManager = new ThreadQueue();
     thread = new pthread_t[NUM_PROCS];
+    instructionsProcessed = 0;
+    
+    loadFile((char *)"1.txt");
+    loadFile((char *)"2.txt");
+    displayMemory();
+    
     
     pthread_barrier_init (&synchroBarrier, NULL, NUM_PROCS);
     printf("Hilo root ejecutandose\n");
