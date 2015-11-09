f4c6a56d9360cfbaa5e7ca4866436e2de5d6a17f
dagokiva <dagokiva@gmail.com>
2015-11-09 16:04:13 -0600

    Nuevo Main.exe de los q hizo blopa de último, y un leve cambio en los batch

--- AutoTestLento.bat
+++ AutoTestLento.bat
@@ -1,2 +1,2 @@
-Main.exe t 30 1 0 Hilos/1.txt Hilos/2.txt Hilos/3.txt Hilos/4.txt Hilos/5.txt Hilos/6.txt
+Main.exe tr 30 1 0 Hilos/1.txt Hilos/2.txt Hilos/3.txt Hilos/4.txt Hilos/5.txt Hilos/6.txt
 PAUSE
