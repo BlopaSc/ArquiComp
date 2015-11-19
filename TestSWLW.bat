83121020814e9eebf05e1d0908cbe524b8bd9f44
BlopaSc <blopa.sauma@hotmail.com>
2015-11-19 16:31:16 -0600

    Hiiiijole, creo que ya funca O_O

--- TestSWLW.bat
+++ TestSWLW.bat
@@ -1,2 +1,2 @@
-Main.exe tr 30 1 0 Hilos3/1.txt Hilos3/2.txt Hilos3/3.txt Hilos3/4.txt Hilos3/5.txt Hilos3/6.txt
+Main.exe fr 30 1 0 Hilos3/1.txt Hilos3/2.txt Hilos3/3.txt Hilos3/4.txt Hilos3/5.txt Hilos3/6.txt
 PAUSE
